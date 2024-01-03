d13ce17ca4c091703090be728f0db371cf7f19bd
LucasEtica <lucas@etica-ti.com>
2024-01-03 13:57:58 -0300

    Ajustes

---
+++ lib/gem_teste_calculadora/calculo.rb
@@ -0,0 +1,23 @@
+module GemTesteCalculadora
+    module Calculo
+      def self.soma(a, b)
+        return a + b
+      end
+  
+      def self.menos(a, b)
+        return a - b
+      end
+  
+      def self.vezes(a, b)
+        return a * b
+      end
+  
+      def self.dividir(a, b)
+        return a / b
+      end
+  
+      def self.multiplicar(a, b)
+        return a * b
+      end
+    end
+  end
